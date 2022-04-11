0414c3890471bbc9243d530b072ff2d190d35c19
amanNbd <amannbd99@gmail.com>
2022-04-11 09:49:43 +0530

    complete-interface

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+// import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "...", //THE API URL
-});
-export default instance;
+// const instance = axios.create({
+//   baseURL: "...", //THE API URL
+// });
+// export default instance;
